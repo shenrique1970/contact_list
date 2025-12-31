19b36ace2fbd618e9d7f81e9bdba939dbe9fae93
shenrique1970 <shvribeiro@hotmail.com>
2025-12-31 06:02:44 -0300

    static pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,18 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root to: "static_pages#index"
+
+  root            to: "static_pages#index"
+  get "sobre",    to: "static_pages#sobre"
+  get "contato",  to: "static_pages#contato"
+
+  get "cadastro", to: "users#new"
+  get "entrar",   to: "sessions#new"
+  post "entrar",  to: "sessions#create"
+  delete "sair",  to: "sessions#destroy"
+
+  resources :users, only: [ :show, :new, :create, :edit, :update ] do
+    resources :contacts, only: [ :index, :new, :create, :edit, :update, :destroy ]
+  end
+  resources :sessions, only: [ :new, :create, :destroy ]
 end
