0d549f2718ae3f3f30c3ed50d5601706c0518ebf
shenrique1970 <shvribeiro@hotmail.com>
2025-12-31 11:45:55 -0300

    rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "users/new"
+  get "users/create"
+  get "users/edit"
+  get "users/update"
+  get "users/show"
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-
-  root to: "static_pages#index"
+  # Páginas estáticas
+  root "static_pages#index"
+  get "home", to: "static_pages#index"
   get "sobre", to: "static_pages#sobre"
   get "contato", to: "static_pages#contato"
 
+  # Autenticação simples
   get "cadastro", to: "users#new"
   get "entrar", to: "sessions#new"
   post "entrar", to: "sessions#create"
   delete "sair", to: "sessions#destroy"
 
+  # Recursos
   resources :users, only: [ :show, :new, :create, :edit, :update ] do
     resources :contacts, only: [ :index, :new, :create, :edit, :update, :destroy ]
   end
+
   resources :sessions, only: [ :new, :create, :destroy ]
+  resources :contacts
 end
