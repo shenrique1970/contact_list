2a8975714c12b6204f24fa8034b41d9833f17da7
shenrique1970 <shvribeiro@hotmail.com>
2026-01-02 13:20:54 -0300

    atualizações contacts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def salvar_e_voltar(form, back_path)
+    content_tag(:div, class: "mt-3") do
+      form.submit("Salvar", class: "btn btn-primary me-2") +
+      link_to("Voltar", back_path, class: "btn btn-secondary")
+    end
+  end
 end
